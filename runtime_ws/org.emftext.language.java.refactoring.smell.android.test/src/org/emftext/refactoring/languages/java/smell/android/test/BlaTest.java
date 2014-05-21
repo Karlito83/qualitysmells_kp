644fc4e57b8a1f3d476ab1fc69f6153636463679
Ronny Marx <karlito1983@googlemail.com>
2014-05-21 14:17:49 +0200

    Intial commit runtime_ws

---
+++ runtime_ws/org.emftext.language.java.refactoring.smell.android.test/src/org/emftext/refactoring/languages/java/smell/android/test/BlaTest.java
@@ -0,0 +1,11 @@
+package org.emftext.refactoring.languages.java.smell.android.test;
+
+public class BlaTest {
+	
+	public BlaTest() {
+		for(int i=0; i<27; i++){
+			System.out.println("Bla!");
+		}
+	}
+
+}
